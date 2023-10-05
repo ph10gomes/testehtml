fc01525e7a39e959cfd5b6893b4df234e6ffb4da
PEDRO HENRIQUE BITTENCOURT GOMES <0049972@IFMG.EDU.BR>
2023-10-05 13:56:14 -0300

    Atividade 9

---
+++ 05-10/index.js
@@ -0,0 +1,84 @@
+const btNovo = document.querySelector('main button');
+const btSalvar = document.querySelector('form button');
+const modal = document.querySelector('div');
+const dados = [];
+let sequencial = 1;
+
+
+
+btNovo.addEventListener('click',() =>{
+   
+    modal.classList.add('mostrarModal');
+});
+
+
+function desenhaTabela(){
+    const tbody = document.querySelector('tbody');
+    tbody.innerHTML = '';
+    for(let i = 0; i < dados.length; i++){
+        const tr = document.createElement('tr');
+
+        const td1 = document.createElement('td');    
+        const td2 = document.createElement('td');    
+        const td3 = document.createElement('td');    
+        const td4 = document.createElement('td');    
+        
+        td1.innerText = dados[i].seq;
+        td2.innerText = dados[i].nome;
+        td3.innerText = dados[i].idade;
+
+        tr.append(td1,td2, td3, td4);
+        tbody.append(tr);
+
+        
+    }
+}
+btSalvar.addEventListener('click',(event) =>{
+    event.preventDefault();
+
+    const registro = {
+         seq:sequencial,
+         nome:document.querySelector('#nome').value,
+         idade:  document.querySelector('#idade').value,
+    };
+
+    dados.push(registro);
+    sequencial = sequencial +1
+
+    desenhaTabela();
+
+    /*
+    const tr = document.createElement('tr');
+    const td1 = document.createElement('td');    
+    const td2 = document.createElement('td');    
+    const td3 = document.createElement('td');    
+    const td4 = document.createElement('td');    
+    const btRemover = document.createElement('button');
+
+     td1.innerText = sequencial;
+     sequencial  = sequencial + 1;
+
+     td2.innerText = document.querySelector('#nome').value;
+     td3.innerText = document.querySelector('#idade').value;
+     
+    btRemover.classList.add('btn-remover');
+    btRemover.innerText = "❌"
+    
+     td4.append(btRemover);
+     
+    btRemover.addEventListener('click',(event) =>{
+       const tr  = event.target.closest('tr')
+        tr.remove();
+       
+    });
+
+     tr.append(td1,td2, td3, td4);
+
+     document.querySelector('tbody').append(tr);
+*/
+    modal.classList.remove('mostrarModal');
+
+    
+
+
+});
